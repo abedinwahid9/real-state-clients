c70f945ec48a46410a79e37b2168051c5c8aaf73
abedin wahid <abedinwahid9@gmail.com>
2023-11-25 19:22:07 +0600

    special section complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Slider from "./Slider/Slider";
+import SpecialSection from "./SpecialSection/SpecialSection";
 
 const Home = () => {
   return (
     <Box>
       <Slider></Slider>
+      <Container sx={{ my: 3 }} maxWidth="xl">
+        <SpecialSection></SpecialSection>
+      </Container>
     </Box>
   );
 };
