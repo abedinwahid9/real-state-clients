bf306388c13df84cd76c7068dfd56ca9ff870bce
abedin wahid <abedinwahid9@gmail.com>
2023-11-29 04:03:55 +0600

    search bar done

--- src/Pages/AllProperties/AllProperties.jsx
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -1,14 +1,30 @@
-import { Box, Container, Grid } from "@mui/material";
-import SearchBar from "../../ShareComponent/SearchBar/SearchBar";
+import { Box, Container, Grid, TextField, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import SectionTitle from "../../Component/SectionTitle/SectionTitle";
 import PropertiesCard from "../../Component/Card/PropertiesCard/PropertiesCard";
 import usePropertise from "../../hooks/usePropertise/usePropertise";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useState, useEffect } from "react";
 
 const AllProperties = () => {
   const [propertise, loading] = usePropertise();
+  const [filteredPropertise, setFilteredPropertise] = useState([]);
 
-  if (loading && !Array.isArray(propertise)) {
+  useEffect(() => {
+    setFilteredPropertise(propertise);
+  }, [propertise]);
+
+  const handleSearch = (query) => {
+    console.log(query);
+    const filtered = propertise.filter(
+      (property) =>
+        property.propertyTitle.toLowerCase().includes(query.toLowerCase()) ||
+        property.propertyLocation.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredPropertise(filtered);
+  };
+
+  if (loading) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center" }}>
         <CircularProgress />
@@ -18,18 +34,35 @@ const AllProperties = () => {
 
   return (
     <Container maxWidth="xl">
-      <SectionTitle title="All Properties"></SectionTitle>
+      <SectionTitle title="All Properties" />
       <Box my={5}>
-        <SearchBar></SearchBar>
+        <TextField
+          label="Search"
+          variant="outlined"
+          size="small"
+          onChange={(e) => handleSearch(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <IconButton
+                style={{
+                  backgroundColor: "#f0f0f0",
+                  borderRadius: "0 4px 4px 0",
+                }}
+                aria-label="search"
+              >
+                <SearchIcon style={{ color: "gray" }} />
+              </IconButton>
+            ),
+            style: { borderRadius: "4px", backgroundColor: "#f0f0f0" },
+          }}
+        />
       </Box>
       <Grid my={5} container spacing={2}>
-        {propertise.map((propertiseItem) => {
-          return (
-            <Grid key={propertiseItem._id} item xs={12} sm={6} md={4}>
-              <PropertiesCard propertiseItem={propertiseItem}></PropertiesCard>
-            </Grid>
-          );
-        })}
+        {filteredPropertise.map((property) => (
+          <Grid key={property._id} item xs={12} sm={6} md={4}>
+            <PropertiesCard propertiseItem={property} />
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
